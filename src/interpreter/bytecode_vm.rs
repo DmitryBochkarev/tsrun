85128b7ea9cf18f25ee30ec011e4e22bbd9ba0f4
Dmitry Bochkarev <dimabochkarev@gmail.com>
2025-12-23 06:14:15 +0100

    Avoid extra clones for accessor setup

--- src/interpreter/bytecode_vm.rs
+++ src/interpreter/bytecode_vm.rs
@@ -3819,14 +3819,14 @@ impl BytecodeVM {
                 setter,
                 is_static,
             } => {
-                let class_val = self.get_reg(class).clone();
+                let class_val = self.get_reg(class);
                 let JsValue::Object(class_obj) = class_val else {
                     return Err(JsError::type_error("Class is not an object"));
                 };
 
-                let getter_val = self.get_reg(getter).clone();
-                let setter_val = self.get_reg(setter).clone();
-                let key_val = self.get_reg(key).clone();
+                let getter_val = self.get_reg(getter);
+                let setter_val = self.get_reg(setter);
+                let key_val = self.get_reg(key);
 
                 // Convert key to string for accessor name
                 let accessor_name = key_val.to_js_string();
@@ -3870,8 +3870,8 @@ impl BytecodeVM {
                 };
 
                 // Merge with existing accessors
-                let final_getter = new_getter.or(existing_getter);
-                let final_setter = new_setter.or(existing_setter);
+                let final_getter = new_getter.cloned().or(existing_getter);
+                let final_setter = new_setter.cloned().or(existing_setter);
 
                 // Create accessor property
                 let property = Property::accessor(final_getter, final_setter);
